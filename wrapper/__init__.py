b488a122f4d9e4453f3450a90fa5d530978a204a
Christopher Woods <chryswoods@gmail.com>
2015-09-13 11:30:01 +0100

    I've added a user-stats uploader that will upload some information to siremol.org about the Sire every time it is run. This can be disabled by setting the environment variable
    SIRE_DONT_PHONEHOME=1
    
    This tries to upload CPU, Sire version, executable and
    git repository information to siremol.org, but does not
    include any user-identifiable information. The upload occurs
    in a background thread and will exit without issue if there
    are any problems. You can see what information has been uploaded
    by inspecting the Sire.sent_usage_data variable after the
    upload has completed. An example is;
    
    {'version': '2015.0.0', 'clockspeed': 2294, 'repository_version': 'f46da231489a2e08d615e70f12a38554ea20ba37/Christopher Woods/2015-09-10T17:55:27+01:00', 'executable': 'ligandswap', 'processor': 'Intel(R) Core(TM) i5-3427U CPU @ 1.80GHz', 'vendor': 'GenuineIntel', 'repository': 'https://github.com/michellab/Sire.git | merge_2014_4', 'numcores': 4}
    
    This information will be useful for Sire developers to see how often
    the code is being used, which processors and version need to be
    supported, and which of the Sire apps are being used.

--- wrapper/__init__.py
+++ wrapper/__init__.py
@@ -15,3 +15,73 @@ import Sire.Error
 import Sire.Config
 
 __version__ = Sire.Config.__version__
+
+sent_usage_data = None
+
+# Now try to upload usage data to siremol.org
+def _uploadUsageData():
+    try:
+        global sent_usage_data
+
+        if not sent_usage_data is None:
+            # don't send data twice
+            return
+
+        import os as _os
+    
+        if "SIRE_DONT_PHONEHOME" in _os.environ:
+           # respect user wish to not phone home
+           return
+
+        from Sire.Base import CPUID as _CPUID
+
+        id = _CPUID()
+
+        data = {}
+
+        data["processor"] = id.brand()
+        data["vendor"] = id.vendor()
+        data["clockspeed"] = id.clockSpeed()
+        data["numcores"] = id.numCores()
+
+        data["version"] = Sire.__version__
+        data["repository"] = Sire.Config.sire_repository_url
+        data["repository_version"] = Sire.Config.sire_repository_version
+
+        import sys as _sys
+        # get the executable name, but make sure we don't get the path
+        # (as it may contain sensitive user information)
+        data["executable"] = _os.path.basename( _sys.executable )
+
+        import json as _json
+
+        import http.client as _htc
+        import urllib.parse as _parse
+
+        params = _parse.urlencode({'data' : _json.dumps(data)})
+        headers = {"Content-type": "application/x-www-form-urlencoded", 
+                   "Accept": "text/plain"}
+
+        #print(_parse.urlencode({'data' : _json.dumps(data)}))
+        #print(headers)
+
+        sent_usage_data = data
+
+        conn = _htc.HTTPConnection("siremol.org")
+        conn.request("POST", "/cgi-bin/postusagestats.php", params, headers)
+        r1 = conn.getresponse()
+        #print(r1.status, r1.reason)
+    except:
+        # something went wrong - just ignore the error
+        # and cancel the phone home
+        return
+
+sent_usage_data = None
+
+if not sent_usage_data:
+    import threading as _threading
+
+    _thread = _threading.Thread(target=_uploadUsageData)
+    _thread.daemon = True
+    _thread.start()
+
