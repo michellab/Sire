760b52c560a0f54ab7c29631b5e7848112ca0a38
Christopher Woods <chryswoods@gmail.com>
2016-01-11 11:15:14 +0000

    Exposing the RanGenerator::detach() function, so that we can easily create exact copies of random number generators (e.g. two generators that will both generate the same sequence of random numbers)
    [ci skip]

--- corelib/src/libs/SireMaths/rangenerator.h
+++ corelib/src/libs/SireMaths/rangenerator.h
@@ -91,6 +91,8 @@ public:
     bool operator==(const RanGenerator &other) const;
     bool operator!=(const RanGenerator &other) const;
 
+    void detach();
+
     void seed();
     void seed(quint32 seed);
     void seed(const QVector<quint32> &seed);
@@ -148,7 +150,6 @@ public:
 
 private:
     detail::RanGeneratorPvt& nonconst_d() const;
-    void detach();
 
     /** Shared pointer to the actual generator */
     boost::shared_ptr<detail::RanGeneratorPvt> d;
