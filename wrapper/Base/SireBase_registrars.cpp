1a6273ef4ed63d29c1f1be3f1e889fe40745339e
Christopher Woods <chryswoods@gmail.com>
2016-04-25 10:57:14 +0100

    Adding in the python wrappers
    Code is working
    
    [ci skip]

--- wrapper/Base/SireBase_registrars.cpp
+++ wrapper/Base/SireBase_registrars.cpp
@@ -3,45 +3,45 @@
 
 #include "SireBase_registrars.h"
 
-#include "majorminorversion.h"
-#include "stringmangler.h"
-#include "properties.h"
 #include "lengthproperty.h"
-#include "linktoproperty.h"
-#include "property.h"
-#include "numberproperty.h"
 #include "propertymap.h"
 #include "propertylist.h"
-#include "cpuid.h"
-#include "stringproperty.h"
+#include "numberproperty.h"
+#include "majorminorversion.h"
 #include "booleanproperty.h"
+#include "stringmangler.h"
+#include "linktoproperty.h"
+#include "stringproperty.h"
+#include "properties.h"
+#include "cpuid.h"
+#include "property.h"
 
 #include "Helpers/objectregistry.hpp"
 
 void register_SireBase_objects()
 {
 
-    ObjectRegistry::registerConverterFor< SireBase::MajorMinorVersion >();
-    ObjectRegistry::registerConverterFor< SireBase::Version >();
-    ObjectRegistry::registerConverterFor< SireBase::NoMangling >();
-    ObjectRegistry::registerConverterFor< SireBase::TrimString >();
-    ObjectRegistry::registerConverterFor< SireBase::UpperCaseString >();
-    ObjectRegistry::registerConverterFor< SireBase::LowerCaseString >();
-    ObjectRegistry::registerConverterFor< SireBase::Properties >();
     ObjectRegistry::registerConverterFor< SireBase::LengthProperty >();
-    ObjectRegistry::registerConverterFor< SireBase::LinkToProperty >();
-    ObjectRegistry::registerConverterFor< SireBase::NullProperty >();
-    ObjectRegistry::registerConverterFor< SireBase::VariantProperty >();
-    ObjectRegistry::registerConverterFor< SireBase::NumberProperty >();
     ObjectRegistry::registerConverterFor< SireBase::PropertyName >();
     ObjectRegistry::registerConverterFor< SireBase::PropertyMap >();
     ObjectRegistry::registerConverterFor< SireBase::DoubleArrayProperty >();
     ObjectRegistry::registerConverterFor< SireBase::IntegerArrayProperty >();
     ObjectRegistry::registerConverterFor< SireBase::StringArrayProperty >();
     ObjectRegistry::registerConverterFor< SireBase::PropertyList >();
-    ObjectRegistry::registerConverterFor< SireBase::CPUID >();
-    ObjectRegistry::registerConverterFor< SireBase::StringProperty >();
+    ObjectRegistry::registerConverterFor< SireBase::NumberProperty >();
+    ObjectRegistry::registerConverterFor< SireBase::MajorMinorVersion >();
+    ObjectRegistry::registerConverterFor< SireBase::Version >();
     ObjectRegistry::registerConverterFor< SireBase::BooleanProperty >();
+    ObjectRegistry::registerConverterFor< SireBase::NoMangling >();
+    ObjectRegistry::registerConverterFor< SireBase::TrimString >();
+    ObjectRegistry::registerConverterFor< SireBase::UpperCaseString >();
+    ObjectRegistry::registerConverterFor< SireBase::LowerCaseString >();
+    ObjectRegistry::registerConverterFor< SireBase::LinkToProperty >();
+    ObjectRegistry::registerConverterFor< SireBase::StringProperty >();
+    ObjectRegistry::registerConverterFor< SireBase::Properties >();
+    ObjectRegistry::registerConverterFor< SireBase::CPUID >();
+    ObjectRegistry::registerConverterFor< SireBase::NullProperty >();
+    ObjectRegistry::registerConverterFor< SireBase::VariantProperty >();
 
 }
 
