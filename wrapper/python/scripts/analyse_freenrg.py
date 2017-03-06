5d3aae03a535a9a69c8c98b8fde38b274d0b5e80
ppxasjsm <antonia.mey@fu-berlin.de>
2017-03-06 15:46:49 +0000

    mbar functionality added

--- wrapper/python/scripts/analyse_freenrg.py
+++ wrapper/python/scripts/analyse_freenrg.py
@@ -1,39 +1,20 @@
-description = """
-analyse_freenrg is an analysis app that has been designed to analyse the output of all free energy calculations in Sire.
- analyse_freenrg reads in a Sire Saved Stream (.s3) file that contains a list of Sire.Analysis free energy objects (e.g.
- FEP, TI, Bennetts). analyse_freenrg will average and analyse these free energies according the to options you supply,
- e.g. assuming that the free energies are stored in freenrgs.s3, and you want to average iterations 100-200 from the
- simulation, and write the results to ‘results.txt’, type;
+"""analyse_freenrg
 
-sire.app/bin/analyse_freenrg -i freenrgs.s3 -r 100 200 -o results.txt
+Usage:
+    analyse_freenrg -h, --help
+    analyse_freenrg  [--description --author --version]
+    analyse_freenrg [-i FILE] [-o FILE] [-g NAME...] [-p NUMBERS] 
 
-Alternatively, if you just want to average over the last 60% of iterations, type;
-
-sire.app/bin/analyse_freenrg -i freenrgs.s3 -o results.txt
-
-(you can specify the percentage to average using the ‘--percent’ option)
-
-analyse_freenrg automatically knows how many free energies are contained in the s3 file, what their types are and what
-should be done to analyse the results. For example, the waterswap, ligandswap and quantomm apps all output s3 files that
-contain FEP, Bennetts and TI free energy data, so analyse_freenrg knows automatically to perform FEP, Bennetts and TI
-analysis on that data and to report all of the results. analyse_freenrg also knows whether or not finite difference
-approximations have been used, whether forwards and backwards windows were evaluated, the temperature and conditions of
-the simulation etc. The aim is that it should handle everything for you, so you can concentrate on looking at the
-potential of mean force (PMF) or final result.
-
-For FEP data, analyse_freenrg will return the FEP PMF across lambda, together with errors based on statistical
-convergence (95% standard error) and the difference between forwards and backwards free energies (if available).
-
-For Bennetts data, analyse_freenrg will return the Bennetts Acceptance Ratio PMF across lambda, with errors based on
-statistical convergence (95% standard error).
-
-For TI data, analyse_free energy will return the PMF across lambda based on polynomial fitting of the gradients and
-analytic integration of the resulting function. It will also return the integral across lambda using simple quadrature.
-Errors are based on statistical convergence (95% standard error) and on the difference between the forwards and
-backwards finite difference gradients (if available, and if finite-difference TI was used).
-
-If you need more help understanding or interpreting the results of an analyse_freenrg analysis then please feel free to
-get in touch via the Sire users mailing list, or by creating a github issue.
+Options:
+    -h, --help                          Show this help message and exit
+    --description                       Print a complete description of this program.
+    --author                            Get information about the authors of this script.
+    --version                           Get version information about this script.
+    -i FILE --input=FILE           Supply the name of the Sire Streamed Save (.s3) file containing the free energies to be analysed.
+    -o FILE --output=FILE          Supply the name of the file in which to write the output.
+    -p NUMBERS --percentage=NUMBERS     Supply the range of iterations over which to average. [default: 60]
+    -g NAME..., --gradients=NAME... Supply the name of the Sire Streamed gradients (.s3) files containing the 
+                                        gradients to be analysed.
 """
 
 from Sire.Analysis import *
@@ -45,8 +26,18 @@ try:
     from Sire.Tools import ap
 except:
     pass
+#try: 
+#    Sire.try_import_from("docopt", "docopt")
+#except:
+#    print ('docopt was not imported sucessfully')
+#    pass
 
-import argparse
+from docopt import docopt
+if __name__ == '__main__':
+    arguments = docopt(__doc__)
+    print (arguments)
+
+"""import argparse
 import sys
 import os
 
@@ -79,7 +70,7 @@ parser.add_argument('-g', '--gradients', nargs='*',
                          "gradients to be analysed.")
 
 parser.add_argument('-o', '--output', nargs=1,
-                    help="""Supply the name of the file in which to write the output.""")
+                    help="Supply the name of the file in which to write the output."")
 
 parser.add_argument('-r', '--range', nargs=2,
                     help="Supply the range of iterations over which to average. "
@@ -108,8 +99,9 @@ args = parser.parse_args()
 
 if args.mbar:
     print (args)
+"""
 
-else:
+"""else:
 
     must_exit = False
 
@@ -118,7 +110,7 @@ else:
         must_exit = True
 
     if args.author:
-        print("\nanalyse_freenrg was written by Christopher Woods (C) 2014")
+        print("\nanalyse_freenrg was written by Christopher Woods and Antonia Mey(C) 2014-2017")
         print("It is based on the analysis tools in Sire.Analysis")
         must_exit = True
 
@@ -380,3 +372,4 @@ else:
     if gradient_files:
         cmd = "rm freenrgs.s3"
         os.system(cmd)
+"""
