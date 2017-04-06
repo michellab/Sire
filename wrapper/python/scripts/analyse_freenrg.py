a2326583e773f3433f4104a22904b5e468367dda
ppxasjsm <antonia.mey@fu-berlin.de>
2017-04-06 14:44:34 +0100

    sorting file list [ci skip]

--- wrapper/python/scripts/analyse_freenrg.py
+++ wrapper/python/scripts/analyse_freenrg.py
@@ -185,7 +185,7 @@ if __name__ == '__main__':
             elif ' ' in arguments['--input'][0]:
                 input_files = arguments['--input'][0].split(' ')
             elif '*' in arguments['--input'][0]:
-                input_files = glob.glob(arguments['--input'][0])
+                input_files = glob.glob(arguments['--input'][0]).sort()
 
         if output_file:
             print("# Writing all output to file %s" % output_file)
