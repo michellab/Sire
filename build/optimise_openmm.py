4de2f037cc1698d810bf3026b1075556e42b443e
ppxasjsm <antonia.mey@fu-berlin.de>
2019-01-23 13:17:52 +0000

    update indent

--- build/optimise_openmm.py
+++ build/optimise_openmm.py
@@ -12,6 +12,7 @@ if 'CUDA' in platforms:
     print('The CUDA platform is successfully recognised by openMM! Nothing needs to be done and you can use SOMD')
     sys.exit(0)
 else:
+    print('CUDA platform is not recognised by OpenMM!')
     # Find OpenMM version
     mm_version = mm.__version__
     print(mm_version)
@@ -31,24 +32,19 @@ else:
     nvcc_out = None
     nvcc_release = None
     nvcc = subprocess.run(["nvcc --version"], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-if nvcc.returncode == 0:
-    nvcc_out = nvcc.stdout.decode("ascii").strip()
-    match = re.search('release [+-]?([0-9]*[.]?[0-9])+', nvcc_out)
-    print(match)
-    nvcc_release = str(match.group().split(' ')[-1])
-    print(type(nvcc_release))
-    print('Found a CUDA toolkit release version: '+nvcc_release)
-else:
-    nvcc_out = nvcc.stderr.decode("ascii").strip()  
-    print('Are you sure CUDA toolkit is installed? nvcc does not seem to exist!')
-    print('If you want to use SOMD with CUDA plese double check your CUDA installation')
-    sys.exit(-1)
+    if nvcc.returncode == 0:
+        nvcc_out = nvcc.stdout.decode("ascii").strip()
+        match = re.search('release [+-]?([0-9]*[.]?[0-9])+', nvcc_out)
+        print(match)
+        nvcc_release = str(match.group().split(' ')[-1])
+        print(type(nvcc_release))
+        print('Found a CUDA toolkit release version: '+nvcc_release)
+    else:
+        nvcc_out = nvcc.stderr.decode("ascii").strip()  
+        print('Are you sure CUDA toolkit is installed? nvcc does not seem to exist!')
+        print('If you want to use SOMD with CUDA plese double check your CUDA installation')
+        sys.exit(-1)
 
-if 'CUDA' in platforms:
-    print('The CUDA platform is successfully recognised by openMM! You can now start using SOMD')
-    sys.exit(0)
-else:
-    print('CUDA platform is not recognised by OpenMM!')
     print('Trying to update OpenMM to match your CUDA version %s for your OpenMM version %s' %(nvcc_release,mm_version))
     version = nvcc_release.replace('.','')
     conda_cmd = conda_exe +'  install --yes --no-deps -c omnia/label/cuda%s openmm=%s' %(version,mm_version)
@@ -63,8 +59,8 @@ else:
             print("Something didn't work out with the update of OpenMM via conda, have a look at the output.")
             print(conda_proc.stdout.decode("ascii".strip()))
             print(conda_proc.stderr.decode("ascii".strip()))
-     else:
-         print('Something went wrong with the conda installation. Please take a look at the ouptut')
-         print('Executing command %s failed' %conda_cmd)
-         print(conda_proc.stderr.decode("ascii".strip()))
+    else:
+        print('Something went wrong with the conda installation. Please take a look at the ouptut')
+        print('Executing command %s failed' %conda_cmd)
+        print(conda_proc.stderr.decode("ascii".strip()))
 
