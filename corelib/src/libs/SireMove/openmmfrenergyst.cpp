50b5e8caf124528eaa55f650c89246741483893c
Sofia Bariami <44237397+SofiaBariami@users.noreply.github.com>
2019-01-28 17:51:26 +0000

    Update openmmfrenergyst.cpp
    Serialise and extract combining rules from binary datastream

--- corelib/src/libs/SireMove/openmmfrenergyst.cpp
+++ corelib/src/libs/SireMove/openmmfrenergyst.cpp
@@ -123,6 +123,7 @@ QDataStream SIREMOVE_EXPORT &operator<<(QDataStream &ds, const OpenMMFrEnergyST 
 
     sds << velver.frequent_save_velocities << velver.molgroup << velver.solute
         << velver.solutehard << velver.solutetodummy << velver.solutefromdummy
+        << velver.CombRules << velver.combining_rules
         << velver.CutoffType << velver.cutoff_distance << velver.field_dielectric
         << velver.Andersen_flag << velver.Andersen_frequency
         << velver.MCBarostat_flag << velver.MCBarostat_frequency
@@ -152,7 +153,8 @@ QDataStream SIREMOVE_EXPORT &operator>>(QDataStream &ds, OpenMMFrEnergyST &velve
         SharedDataStream sds(ds);
         sds >> velver.frequent_save_velocities >> velver.molgroup
             >> velver.solute >> velver.solutehard >> velver.solutetodummy
-            >> velver.solutefromdummy >> velver.CutoffType >> velver.cutoff_distance
+            >> velver.solutefromdummy >> velver.CombRules >> velver.combining_rules 
+            >> velver.CutoffType >> velver.cutoff_distance
             >> velver.field_dielectric >> velver.Andersen_flag
             >> velver.Andersen_frequency >> velver.MCBarostat_flag
             >> velver.MCBarostat_frequency >> velver.ConstraintType
