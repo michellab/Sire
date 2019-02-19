af7177f47cfce9eaba4336d57185eca09a6fbb38
Paolo Tosco <paolo.tosco.mail@gmail.com>
2019-02-19 12:24:12 +0000

    - clean current arguments when calling activate

--- compile_sire.sh
+++ compile_sire.sh
@@ -183,7 +183,7 @@ if [ ! -z "$CONDA_PYTHON" ]; then
     CONDA_BINDIR="`dirname "$CONDA_PYTHON"`"
     echo "** Running the conda activate script... **"
     echo "** . \"$CONDA_BINDIR/activate\""
-    . "$CONDA_BINDIR/activate"
+    source "$CONDA_BINDIR/activate" ""
     echo "** Running the Python install script... **"
     echo "** \"$CONDA_PYTHON\" build/build_sire.py **"
     "$CONDA_PYTHON" build/build_sire.py
