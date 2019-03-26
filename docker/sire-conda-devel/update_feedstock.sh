802c5a9472a313f890c597e1a4a534d4687e8bcc
Lester Hedges <lester.hedges@gmail.com>
2019-03-26 09:49:55 +0000

    Only update feedstock during Linux build.

--- docker/sire-conda-devel/update_feedstock.sh
+++ docker/sire-conda-devel/update_feedstock.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-# Install Git into the Sire miniconda. The default installed on the macOS
-# build image is too old to support username:token https authentication.
-$HOME/sire.app/bin/conda install -y git
-GIT=$HOME/sire.app/bin/git
-
 # Get the GitHub token and email.
 GITHUB_TOKEN=${GITHUB_TOKEN:-$1}
 GITHUB_EMAIL=${GITHUB_EMAIL:-$2}
@@ -14,6 +9,7 @@ CONDA_DIR=$HOME/staged-recipes
 
 # Delete any existing Conda Forge directory.
 if [ -d $CONDA_DIR ]; then
+    echo "Deleting existing Conda directory: '$CONDA_DIR'"
     rm -rf $CONDA_DIR
 fi
 
@@ -22,67 +18,70 @@ RECIPE=$CONDA_DIR/recipes/sire/meta.yaml
 TEMPLATE=$CONDA_DIR/recipes/sire/template.yaml
 
 # Clone the feedstock repository.
-$GIT clone --branch devel https://github.com/michellab/staged-recipes.git $CONDA_DIR > /dev/null 2>&1
+echo "Cloning Conda Forge feedstock: 'https://github.com/michellab/staged-recipes.git'"
+git clone --branch devel https://github.com/michellab/staged-recipes.git $CONDA_DIR > /dev/null 2>&1
 
 # Overwite the recipe with the template file.
 cp $TEMPLATE $RECIPE
 
-# MacOS.
-if [ "$(uname)" = "Darwin" ]; then
-    ARCHIVE=$HOME/sire_conda_latest_osx.tar.bz2
-    SHA256=SHA256_OSX
+ARCHIVE_LINUX=$HOME/sire_conda_latest_linux.tar.bz2
+ARCHIVE_OSX=$HOME/sire_conda_latest_osx.tar.bz2
 
-# Linux.
-elif [ "$(uname)" = "Linux" ]; then
-    ARCHIVE=$HOME/sire_conda_latest_linux.tar.bz2
-    SHA256=SHA256_LINUX
+# Download the macOS Conda package files. The macOS Azure build is much faster
+# than the Linux one so these files should always be created by the time we
+# get to this stage. If not, then the macOS Conda package will be one commit
+# behind the Linux package.
+echo "Downloading macOS Conda package file"
+curl --silent --insecure --location https://objectstorage.eu-frankfurt-1.oraclecloud.com/p/LSe3OL7yKxPq2d5BgBVrvWFWdFlMzBG4VKUbbMahXMU/n/chryswoods/b/sire_releases/o/sire_conda_latest_osx.tar.bz2 --output $HOME/sire_conda_latest_osx.tar.bz2
 
-    # List of python dependencies.
-    DEPS=(boost gsl netcdf4 openmm pyqt tbb tbb-dev)
+# List of python dependencies.
+DEPS=(boost gsl netcdf4 openmm pyqt tbb tbb-dev)
 
-    # Where the Conda environment is stored.
-    CONDA_ENV=.conda_env
+# Where the Conda environment is stored.
+CONDA_ENV=.conda_env
 
-    # Get the Sire version.
-    SIRE_VER=$($GIT --git-dir=$HOME/Sire/.git describe --tags)
+# Get the Sire version.
+SIRE_VER=$(git --git-dir=$HOME/Sire/.git describe --tags)
 
-    # Store the conda environment.
-    $HOME/sire.app/bin/conda env export -n base > $CONDA_ENV
+# Store the conda environment.
+$HOME/sire.app/bin/conda env export -n base > $CONDA_ENV
 
-    # Loop over all dependences and replace with the version installed
-    # within the Conda enviroment.
-    for dep in ${DEPS[@]}; do
-        ver=$(grep "\- $dep=" $CONDA_ENV | awk -F "=" '{print $2}')
-        sed -i.bak -e "s/$dep/$dep $ver/" -- $RECIPE && rm -- $RECIPE.bak
-    done
+# Loop over all dependences and replace with the version installed
+# within the Conda enviroment.
+echo "Updating Python dependencies..."
+for dep in ${DEPS[@]}; do
+    ver=$(grep "\- $dep=" $CONDA_ENV | awk -F "=" '{print $2}')
+    sed -i.bak -e "s/$dep/$dep $ver/" -- $RECIPE && rm -- $RECIPE.bak
+    echo "  $dep $ver"
+done
 
-    # Update the Sire version number.
-    sed -i.bak -e "s/VERSION/$SIRE_VER/" -- $RECIPE && rm -- $RECIPE.bak
+# Update the Sire version number.
+echo "Updating Sire version number: '$SIRE_VER'"
+sed -i.bak -e "s/VERSION/$SIRE_VER/" -- $RECIPE && rm -- $RECIPE.bak
 
-    # Remove the Conda environment file.
-    rm -f .conda_env
+# Remove the Conda environment file.
+rm -f .conda_env
 
-# Unsupported OS.
-else
-    echo "Unsupported operating system: '$uname'"
-    exit -1
-fi
-
-# Update the sha256 checksum.
-CHECKSUM=$(openssl sha256 $ARCHIVE | awk '{print $2}')
-sed -i.bak -e "s/$SHA256/$CHECKSUM/" -- $RECIPE && rm -- $RECIPE.bak
+# Update the sha256 checksums.
+CHECKSUM_LINUX=$(openssl sha256 $ARCHIVE_LINUX | awk '{print $2}')
+CHECKSUM_OSX=$(openssl sha256 $ARCHIVE_OSX | awk '{print $2}')
+echo "Updating package checksums..."
+echo "  Linux: $CHECKSUM_LINUX"
+echo "  macOS: $CHECKSUM_OSX"
+sed -i.bak -e "s/SHA256_LINUX/$CHECKSUM_LINUX/" -- $RECIPE && rm -- $RECIPE.bak
+sed -i.bak -e "s/SHA256_OSX/$CHECKSUM_OSX/" -- $RECIPE && rm -- $RECIPE.bak
 
 # Remove the recipe backup file.
 rm -f $RECIPE.bak
 
 # Change to Conda package directory and update git config.
 cd $CONDA_DIR
-$GIT config user.name "BioSimSpaceBot"
-$GIT config user.email "$GITHUB_EMAIL"
+git config user.name "BioSimSpaceBot" > /dev/null 2>&1
+git config user.email "$GITHUB_EMAIL" > /dev/null 2>&1
 
-# Commit the changes to the Conda recipe. Make sure to pull and rebase before
-# pushing to avoid conflicts in the unlikely event that the Linux and macOS
-# builds make simultaneous edits.
-$GIT commit -a -m "Updating Conda recipe."
-$GIT pull --rebase origin devel
-$GIT push --repo https://biosimspacebot:$GITHUB_TOKEN@github.com/michellab/staged-recipes.git origin devel
+# Commit the changes to the Conda recipe.
+echo "Commiting changes to feedstock"
+git commit -a -m "Updating Conda recipe." > /dev/null 2>&1
+echo "Pushing changes to remote"
+git push --repo https://biosimspacebot:$GITHUB_TOKEN@github.com/michellab/staged-recipes.git origin devel > /dev/null 2>&1
+echo "Feedstock updated!"
