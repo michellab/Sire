dfbaa23397ed6e9f700e088bf3bb79b4261491c2
Sofia Bariami <44237397+SofiaBariami@users.noreply.github.com>
2019-04-15 10:47:17 +0100

    Update somd-freenrg.py

--- wrapper/python/scripts/somd-freenrg.py
+++ wrapper/python/scripts/somd-freenrg.py
@@ -22,7 +22,7 @@ parser = argparse.ArgumentParser(description="Perform molecular dynamics single 
 parser.add_argument('-C', '--config', nargs="?", 
                     help='Supply an optional CONFIG file to control the calculation.')
 
-parser.add_argument('-r', '--restr_atoms', nargs="?",
+parser.add_argument('-r', '--restrained_atoms_file', nargs="?",
                     help="A txt file with the atom numbers of the ones to be restrained ")
 
 parser.add_argument('-H', '--help-config', action="store_true",
@@ -132,9 +132,9 @@ if args.lambda_val:
     lambda_val = float(args.lambda_val)
     params["lambda_val"] = lambda_val
 
-if args.restr_atoms:
-	restr_atoms = args.restr_atoms
-	params["restrained_atoms"] = restr_atoms
+if args.restrained_atoms_file:
+    res_atoms_file = args.restrained_atoms_file
+    params["rstr_atoms_file"] = res_atoms_file
 
 if not (os.path.exists(coord_file) and os.path.exists(top_file) and os.path.exists(morph_file)):
     parser.print_help()
