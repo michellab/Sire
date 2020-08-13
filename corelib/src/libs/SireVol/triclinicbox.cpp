abe18f60c5df5ae3cd666e49c014fa6d72485678
Lester Hedges <lester.hedges@gmail.com>
2020-08-13 16:44:09 +0100

    Started implementing support for triclinic simulation boxes.

---
+++ corelib/src/libs/SireVol/triclinicbox.cpp
@@ -0,0 +1,394 @@
+/********************************************\
+  *
+  *  Sire - Molecular Simulation Framework
+  *
+  *  Copyright (C) 2007  Christopher Woods
+  *
+  *  This program is free software; you can redistribute it and/or modify
+  *  it under the terms of the GNU General Public License as published by
+  *  the Free Software Foundation; either version 2 of the License, or
+  *  (at your option) any later version.
+  *
+  *  This program is distributed in the hope that it will be useful,
+  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  *  GNU General Public License for more details.
+  *
+  *  You should have received a copy of the GNU General Public License
+  *  along with this program; if not, write to the Free Software
+  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+  *
+  *  For full details of the license please see the COPYING file
+  *  that should have come with this distribution.
+  *
+  *  You can contact the authors via the developer's mailing list
+  *  at http://siremol.org
+  *
+\*********************************************/
+
+#include <QDebug>
+#include <QList>
+#include <QMutex>
+#include <QPair>
+
+#include <limits>
+#include <cmath>
+
+#ifdef SIRE_USE_SSE
+    #ifdef __SSE__
+        #include <emmintrin.h>   // CONDITIONAL_INCLUDE
+    #else
+        #undef SIRE_USE_SSE
+    #endif
+#endif
+
+#include "triclinicbox.h"
+#include "coordgroup.h"
+
+#include "SireMaths/rangenerator.h"
+
+#include "SireBase/countflops.h"
+
+#include "SireError/errors.h"
+#include "SireStream/datastream.h"
+
+#include <QDebug>
+
+// Helper struct for sorting based on first pair value.
+struct QPairFirstComparer
+{
+    template<typename T1, typename T2>
+    bool operator()(const QPair<T1,T2> & a, const QPair<T1,T2> & b) const
+    {
+        return a.first < b.first;
+    }
+};
+
+using namespace SireVol;
+using namespace SireBase;
+using namespace SireMaths;
+using namespace SireStream;
+
+static const RegisterMetaType<TriclinicBox> r_box;
+
+/** Serialise to a binary datastream */
+QDataStream &operator<<(QDataStream &ds, const TriclinicBox &box)
+{
+    writeHeader(ds, r_box, 1)
+               << box.v0_orig
+               << box.v1_orig
+               << box.v2_orig;
+
+               //no need to store anything else as it can be regenerated
+
+    return ds;
+}
+
+/** Deserialise from a binary datastream */
+QDataStream &operator>>(QDataStream &ds, TriclinicBox &box)
+{
+    VersionID v = readHeader(ds, r_box);
+
+    if (v == 1)
+    {
+        Vector v0, v1, v2;
+
+        // Load in the original box vectors.
+        ds >> v0 >> v1 >> v2;
+
+        // Reconstruct the box.
+        box = TriclinicBox(v0, v1, v2);
+    }
+    else
+        throw version_error(v, "1", r_box, CODELOC);
+
+    return ds;
+}
+
+/** This is the maximum dimension of the box (so that .volume() doesn't overflow) */
+const double max_boxlength = std::pow(0.9 * std::numeric_limits<double>::max(), 1.0/3.0);
+
+/** Construct a default TriclinicBox volume (maximum volume) */
+TriclinicBox::TriclinicBox() : ConcreteProperty<TriclinicBox,Cartesian>()
+{
+    Vector v0(max_boxlength, 0, 0);
+    Vector v1(0, max_boxlength, 0);
+    Vector v2(0, 0, max_boxlength);
+
+    *this = TriclinicBox(v0, v1, v2);
+}
+
+/** Construct a TriclinicBox with the specified lattice vectors */
+TriclinicBox::TriclinicBox(const Vector &v0,
+                           const Vector &v1,
+                           const Vector &v2)
+            : ConcreteProperty<TriclinicBox,Cartesian>(),
+              v0(v0),
+              v1(v1),
+              v2(v2),
+              v0_orig(v0),
+              v1_orig(v1),
+              v2_orig(v2)
+{
+    // What follows was adapted from boxmtx.pl by Tsjerk A. Wassenaar.
+
+    // Get the magnitudes of lattice box vectors.
+    auto m0 = v0.magnitude();
+    auto m1 = v1.magnitude();
+    auto m2 = v2.magnitude();
+
+    // Create a list containing of magnitude/vector pairs.
+    QList<QPair<double, Vector> > magnitudes;
+    magnitudes.append(qMakePair(m0, v0));
+    magnitudes.append(qMakePair(m1, v1));
+    magnitudes.append(qMakePair(m2, v2));
+
+    // Sort the pairs in ascending order.
+    qSort(magnitudes.begin(), magnitudes.end(), QPairFirstComparer());
+
+    // Reorder the vectors in order of descending magnitude.
+    this->v0 = magnitudes[2].second;
+    this->v1 = magnitudes[1].second;
+    this->v2 = magnitudes[0].second;
+
+    // Project v1 onto v0.
+    auto xn1 = Vector::dot(v0, v1) / m0;
+
+    // Evaluate the square root of the difference between the length of v1 squared
+    // and the projection of that vector on v0 squared.
+    auto yn1 = sqrt(m1*m1 - xn1*xn1);
+
+    // Evaluate the vector product of v0 and v1 and take the magnitude.
+    auto zn2 = Vector::realCross(v0, v1).magnitude();
+
+    // Construct the original matrix.
+    Matrix X = Matrix(v0, v1, Vector::realCross(v0, v1)).transpose();
+
+    // Constuct the rotated matrix.
+    Matrix X_rot(Vector(m0, xn1, 0),
+                 Vector(0, yn1, 0),
+                 Vector(0, 0, zn2));
+
+    // Evaluate and store the rotation matrix.
+    this->rotation_matrix = X_rot * X.inverse();
+
+    // Rotate the lattice vectors.
+    this->v0 = this->rotation_matrix * v0;
+    this->v1 = this->rotation_matrix * v1;
+    this->v2 = this->rotation_matrix * v2;
+
+    // Make sure v2 has a positive z component.
+    if (this->v2.z() < 0)
+        this->v2 *= -1;
+
+    /* Next perform a lattice reduction such that the following conditions are
+       met:
+
+       | v1.x | <= 1/2 v0.x
+       | v2.x | <= 1/2 v0.x
+       | v2.y | <= 1/2 v1.y
+
+       These constraints can be solved using simultaneous equations.
+     */
+
+    // v1.x
+    double v1x;
+    if (this->v1.x() != 0)
+    {
+        v1x = this->v1.x()
+            - int((this->v1.x() / this->v0.x())
+            + (this->v1.x() / (2.0 * qAbs(this->v1.x()))))
+            * this->v0.x();
+    }
+    else
+    {
+        v1x = this->v1.x();
+    }
+
+    // v2.x
+    double v2x;
+    if (this->v2.x() != 0)
+    {
+        v2x = this->v2.x()
+            - int((this->v2.x() / this->v1.x())
+            + (this->v2.x() / (2.0 * qAbs(this->v2.x()))))
+            * this->v1.x();
+    }
+    else
+    {
+        v2x = this->v2.x();
+    }
+
+    // v2.y
+    double v2y;
+    if (this->v2.y() != 0)
+    {
+        v2y = this->v2.y()
+            - int((this->v2.y() / this->v1.y())
+            + (this->v2.y() / (2.0 * qAbs(this->v2.y()))))
+            * this->v1.y();
+    }
+    else
+    {
+        v2y = this->v2.y();
+    }
+
+    // Update the lattice vectors.
+    this->v1 = Vector(v1x, this->v1.y(), this->v1.z());
+    this->v2 = Vector(v2x, v2y, this->v2.z());
+}
+
+/** Copy constructor */
+TriclinicBox::TriclinicBox(const TriclinicBox &other)
+            : ConcreteProperty<TriclinicBox,Cartesian>(other),
+              v0(other.v0),
+              v1(other.v1),
+              v2(other.v2),
+              v0_orig(other.v0_orig),
+              v1_orig(other.v1_orig),
+              v2_orig(other.v2_orig),
+              rotation_matrix(other.rotation_matrix)
+{}
+
+/** Destructor */
+TriclinicBox::~TriclinicBox()
+{}
+
+/** Copy assignment operator */
+TriclinicBox& TriclinicBox::operator=(const TriclinicBox &other)
+{
+    if (this != &other)
+    {
+        v0 = other.v0;
+        v1 = other.v1;
+        v2 = other.v2;
+        v0_orig = other.v0_orig;
+        v1_orig = other.v1_orig;
+        v2_orig = other.v2_orig;
+        rotation_matrix = other.rotation_matrix;
+        Cartesian::operator=(other);
+    }
+
+    return *this;
+}
+
+/** Comparison operator */
+bool TriclinicBox::operator==(const TriclinicBox &other) const
+{
+    return v0 == other.v0 and
+           v1 == other.v1 and
+           v2 == other.v2 and
+           v0_orig == other.v0_orig and
+           v1_orig == other.v1_orig and
+           v2_orig == other.v2_orig and
+           rotation_matrix == rotation_matrix;
+}
+
+/** Comparison operator */
+bool TriclinicBox::operator!=(const TriclinicBox &other) const
+{
+    return v0 != other.v0 or
+           v1 != other.v1 or
+           v2 != other.v2 or
+           v0_orig != other.v0_orig or
+           v1_orig != other.v1_orig or
+           v2_orig != other.v2_orig or
+           rotation_matrix != rotation_matrix;
+}
+
+/** A Triclinic box is periodic! */
+bool TriclinicBox::isPeriodic() const
+{
+    return true;
+}
+
+/** A Triclinic box is cartesian */
+bool TriclinicBox::isCartesian() const
+{
+    return true;
+}
+
+/** Return the first box vector */
+const Vector& TriclinicBox::vector0() const
+{
+    return v0;
+}
+
+/** Return the second box vector */
+const Vector& TriclinicBox::vector1() const
+{
+    return v1;
+}
+
+/** Return the third box vector */
+const Vector& TriclinicBox::vector2() const
+{
+    return v2;
+}
+
+/** Return the rotation matrix */
+const Matrix& TriclinicBox::rotationMatrix() const
+{
+    return rotation_matrix;
+}
+
+/** Return the cell matrix */
+Matrix TriclinicBox::cellMatrix() const
+{
+    Matrix M = Matrix(v0, v1, v2).transpose();
+    return M;
+}
+
+/** Return a cubic TriclinicBox with image distance d. */
+TriclinicBox TriclinicBox::cubic(double d)
+{
+    Vector v0(d, 0, 0);
+    Vector v1(0, d, 0);
+    Vector v2(0, 0, d);
+
+    return TriclinicBox(v0, v1, v2);
+}
+
+/** Return a square rhombic dodecahedron TriclinicBox with image distance d. */
+TriclinicBox TriclinicBox::rhombicDodecahedronSquare(double d)
+{
+    Vector v0(d, 0, 0);
+    Vector v1(0, d, 0);
+    Vector v2(0.5*d, 0.5*d, 0.5*sqrt(2)*d);
+
+    return TriclinicBox(v0, v1, v2);
+}
+
+/** Return a hexagonal rhombic dodecahedron TriclinicBox with image distance d. */
+TriclinicBox TriclinicBox::rhombicDodecahedronHexagon(double d)
+{
+    Vector v0(d, 0, 0);
+    Vector v1(0.5, 0.5*sqrt(3)*d, 0);
+    Vector v2(0.5*d, (1/6.0)*sqrt(3)*d, (1/3.0)*sqrt(6)*d);
+
+    return TriclinicBox(v0, v1, v2);
+}
+
+/** Return a truncated octahedron with image distance d. */
+TriclinicBox TriclinicBox::truncatedOctahedron(double d)
+{
+    Vector v0(d, 0, 0);
+    Vector v1(d/3.0, (2/3.0)*sqrt(2)*d, 0);
+    Vector v2(-d/3.0, (1/3.0)*sqrt(2)*d, (1/3.0)*sqrt(6)*d);
+
+    return TriclinicBox(v0, v1, v2);
+}
+
+/** Return a string representation of this space */
+QString TriclinicBox::toString() const
+{
+    return QObject::tr("TriclinicBox( %1, %2, %3 )")
+                .arg( this->vector0().toString() )
+                .arg( this->vector1().toString() )
+                .arg( this->vector2().toString() );
+}
+
+const char* TriclinicBox::typeName()
+{
+    return QMetaType::typeName( qMetaTypeId<TriclinicBox>() );
+}
---
+++ corelib/src/libs/SireVol/triclinicbox.h
@@ -0,0 +1,148 @@
+/********************************************\
+  *
+  *  Sire - Molecular Simulation Framework
+  *
+  *  Copyright (C) 2006  Christopher Woods
+  *
+  *  This program is free software; you can redistribute it and/or modify
+  *  it under the terms of the GNU General Public License as published by
+  *  the Free Software Foundation; either version 2 of the License, or
+  *  (at your option) any later version.
+  *
+  *  This program is distributed in the hope that it will be useful,
+  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  *  GNU General Public License for more details.
+  *
+  *  You should have received a copy of the GNU General Public License
+  *  along with this program; if not, write to the Free Software
+  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+  *
+  *  For full details of the license please see the COPYING file
+  *  that should have come with this distribution.
+  *
+  *  You can contact the authors via the developer's mailing list
+  *  at http://siremol.org
+  *
+\*********************************************/
+
+#ifndef SIREVOL_TRICLINICBOX_H
+#define SIREVOL_TRICLINICBOX_H
+
+#include "cartesian.h"
+
+#include "SireMaths/matrix.h"
+
+#include <QDebug>
+
+SIRE_BEGIN_HEADER
+
+namespace SireVol
+{
+class TriclinicBox;
+}
+
+SIREVOL_EXPORT QDataStream& operator<<(QDataStream&, const SireVol::TriclinicBox&);
+SIREVOL_EXPORT QDataStream& operator>>(QDataStream&, SireVol::TriclinicBox&);
+
+namespace SireVol
+{
+
+using SireMaths::Matrix;
+using SireMaths::Vector;
+
+/**
+A TriclinicBox is a volume  that represents standard periodic boundary conditions
+(a 3D box replicated to infinity along all three dimensions).
+
+@author Christopher Woods
+*/
+class SIREVOL_EXPORT TriclinicBox
+        : public SireBase::ConcreteProperty<TriclinicBox,Cartesian>
+{
+
+friend SIREVOL_EXPORT QDataStream& ::operator<<(QDataStream&, const TriclinicBox&);
+friend SIREVOL_EXPORT QDataStream& ::operator>>(QDataStream&, TriclinicBox&);
+
+public:
+    TriclinicBox();
+    TriclinicBox(const Vector &v0, const Vector &v1, const Vector &v2);
+
+    TriclinicBox(const TriclinicBox &other);
+
+    ~TriclinicBox();
+
+    TriclinicBox& operator=(const TriclinicBox &other);
+
+    bool operator==(const TriclinicBox &other) const;
+    bool operator!=(const TriclinicBox &other) const;
+
+    bool isPeriodic() const;
+    bool isCartesian() const;
+
+    QString toString() const;
+
+    static const char* typeName();
+
+    /** Return the first box vector. */
+    const Vector& vector0() const;
+
+    /** Return the second box vector. */
+    const Vector& vector1() const;
+
+    /** Return the third box vector. */
+    const Vector& vector2() const;
+
+    /** Return the rotation matrix. */
+    const Matrix& rotationMatrix() const;
+
+    /** Return the cell matrix. */
+    Matrix cellMatrix() const;
+
+    /** Return a cubic TriclinicBox with image distance d. */
+    static TriclinicBox cubic(double d);
+
+    /** Return a square rhombic dodecahedron TriclinicBox with image distance d. */
+    static TriclinicBox rhombicDodecahedronSquare(double d);
+
+    /** Return a hexagonal rhombic dodecahedron TriclinicBox with image distance d. */
+    static TriclinicBox rhombicDodecahedronHexagon(double d);
+
+    /** Return a truncated octahedron with image distance d. */
+    static TriclinicBox truncatedOctahedron(double d);
+
+protected:
+
+    /** The first box vector */
+    Vector v0;
+
+    /** The second box vector */
+    Vector v1;
+
+    /** The third box vector */
+    Vector v2;
+
+    /** The first box vector (original copy) */
+    Vector v0_orig;
+
+    /** The second box vector (original copy) */
+    Vector v1_orig;
+
+    /** The third box vector (original copy) */
+    Vector v2_orig;
+
+    /** The rotation matrix used to transform the box to meet the requirements
+        of molecular dynamics engines.
+      */
+    Matrix rotation_matrix;
+};
+
+}
+
+Q_DECLARE_METATYPE(SireVol::TriclinicBox)
+
+SIRE_EXPOSE_CLASS( SireVol::TriclinicBox )
+
+SIRE_END_HEADER
+
+#endif
