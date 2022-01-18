5a2aa2b209c3242ee4047b688127877657ba06ac
Lester Hedges <lester.hedges@gmail.com>
2022-01-18 10:43:26 +0000

    Add support for LangevinMiddleIntegrator. [ref #362] ***NO_CI***

--- corelib/src/libs/SireMove/openmmmdintegrator.cpp
+++ corelib/src/libs/SireMove/openmmmdintegrator.cpp
@@ -1109,6 +1109,8 @@ void OpenMMMDIntegrator::createContext(IntegratorWorkspace &workspace,
             integrator_openmm = new OpenMM::VariableVerletIntegrator(integration_tol); //integration tolerance error unitless
         else if (Integrator_type == "langevin")
             integrator_openmm = new OpenMM::LangevinIntegrator(converted_Temperature, converted_friction, dt);
+        else if (Integrator_type == "langevinmiddle")
+            integrator_openmm = new OpenMM::LangevinMiddleIntegrator(converted_Temperature, converted_friction, dt);
         else if (Integrator_type == "variablelangevin")
             integrator_openmm = new OpenMM::VariableLangevinIntegrator(converted_Temperature, converted_friction, integration_tol);
         else if (Integrator_type == "brownian")
