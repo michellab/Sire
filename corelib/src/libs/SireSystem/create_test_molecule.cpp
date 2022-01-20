4206a348fd12b6fd9a96e14ab6409dd381a79216
Christopher Woods <Christopher.Woods@bristol.ac.uk>
2022-01-20 18:01:18 +0000

    Fixed the MacOS Property wrapping issue in a hacky way. Essentially, you have to force the code to create and manipulate Property types before they are created elsewhere. So we do this in _fix_atomproperty_types() which is called as soon as Sire.IO is imported.

--- corelib/src/libs/SireSystem/create_test_molecule.cpp
+++ corelib/src/libs/SireSystem/create_test_molecule.cpp
@@ -5,6 +5,9 @@
 #include "SireMol/atomcharges.h"
 #include "SireMol/atommasses.h"
 #include "SireMol/cgname.h"
+#include "SireMol/resname.h"
+#include "SireMol/chainname.h"
+#include "SireMol/segname.h"
 #include "SireMol/atomname.h"
 #include "SireMol/mover.hpp"
 #include "SireMol/moleditor.h"
@@ -18,7 +21,13 @@ namespace SireSystem
     Molecule create_test_molecule()
     {
         auto m = Molecule();
-        m = m.edit().add(CGName("0")).add(AtomName("H")).molecule().commit();
+        m = m.edit().add(CGName("0")).add(AtomName("H")).molecule()
+                    .add(ChainName("A"))
+                    .add(ResName("H")).molecule()
+                    .add(SegName("A")).molecule()
+                    .atom(AtomName("H"))
+                    .reparent(ResName("H")).reparent(SegName("A")).molecule()
+                    .commit();
 
         return m;
     }
