0524ec2bdaa1157c1ed056551cb794342ea48709
Christopher Woods <Christopher.Woods@bristol.ac.uk>
2022-06-14 13:41:57 +0100

    Got a full build working with the new setup.py script.
    Need to test on lots of different platforms

--- setup.py
+++ setup.py
@@ -17,9 +17,9 @@ USAGE:
 
 import sys
 import os
-import shutil
 import platform
 import subprocess
+import glob
 
 # We can only run this script from the sire directory
 curdir = os.path.abspath(".")
@@ -178,7 +178,10 @@ _is_conda_prepped = False
 
 def conda_install(dependencies):
     """Install the passed list of dependencies using conda"""
-    conda_exe = conda
+    if mamba is None:
+        conda_exe = conda
+    else:
+        conda_exe = mamba
 
     global _is_conda_prepped
 
@@ -197,6 +200,30 @@ def conda_install(dependencies):
         if status.returncode != 0:
             print("Failed to set channel priority!")
             sys.exit(-1)
+
+        if is_macos and machine == "arm64":
+            # Now update conda - this is needed to fix libffi compatibility
+            # errors that break conda
+            cmd = [conda_exe, "update", "-y", "-n", "base",
+                   "-c", "defaults", "conda"]
+            print("Updating conda base using: '%s'" % " ".join(cmd))
+            status = subprocess.run(cmd)
+
+            if status.returncode != 0:
+                print("Something went wrong with the update!")
+                sys.exit(-1)
+
+            # Need to run this command to prevent conda errors on
+            # some platforms - see
+            # https://github.com/ContinuumIO/anaconda-issues/issues/11246
+            # If we don't do this, then we can't resolve dependencies
+            # on MacOS M1
+            cmd = "%s config --set channel_priority false" % conda_exe
+            print("Setting channel priority to false using: '%s'" % cmd)
+            status = subprocess.run(cmd.split())
+            if status.returncode != 0:
+                print("Failed to set channel priority!")
+                sys.exit(-1)
 
         _is_conda_prepped = True
 
@@ -276,12 +303,308 @@ def install_requires():
     conda_install(dependencies)
 
 
-def build():
+def add_default_cmake_defs(cmake_defs, ncores):
+    for a in ("ANACONDA_BASE=%s" % conda_base.replace("\\", "/"),
+              "BUILD_NCORES=%s" % ncores):
+        found = False
+        for d in cmake_defs:
+            if (a in d[0]):
+                found = True
+                break
+        if (not found):
+            cmake_defs.append([a])
+
+    if is_macos:
+        # don't compile with AVX as the resulting binaries won't
+        # work on M1 macs
+        cmake_defs.append(["SIRE_DISABLE_AVX=ON"])
+        cmake_defs.append(["SIRE_DISABLE_AVX512F=ON"])
+
+
+def make_cmd(ncores, install = False):
+    if is_windows:
+        action = "INSTALL" if install else "ALL_BUILD"
+        make_args = "%s -- /m:%s /p:Configuration=Release /p:Platform=x64" % (action, ncores)
+    else:
+        action = "install" if install else "all"
+        make_args = "%s -- VERBOSE=1 -j %s" % (action, ncores)
+
+    return make_args.split()
+
+
+def _get_build_ext():
+    return os.path.basename(conda_base.replace(" ", "_"))
+
+
+def build(ncores: int = 1, npycores: int = 1,
+          coredefs=[], pydefs=[]):
     print("\nCompiling the C++ code")
 
+    CC=None
+    CXX=None
 
-def install():
+    cmake = "cmake"
+
+    if is_windows:
+        cmake = f"{cmake}.exe"
+
+    cmake = os.path.join(conda_bin, cmake)
+
+    # get the compilers
+    if is_macos:
+        try:
+            CXX = glob.glob(os.path.join(conda_bin, "clang++"))[0]
+            CC = glob.glob(os.path.join(conda_bin, "clang"))[0]
+        except:
+            print("Cannot find the conda clang++ binaries!")
+            sys.exit(-1)
+    elif is_linux:
+        try:
+            CXX = glob.glob(os.path.join(conda_bin, "*-g++"))[0]
+            CC = glob.glob(os.path.join(conda_bin, "*-gcc"))[0]
+        except:
+            print("Cannot find the conda g++ binaries!")
+            sys.exit(-1)
+
+    print("Using compilers %s | %s" % (CC, CXX))
+
+    # Make sure all of the above output is printed to the screen
+    # before we start running any actual compilation
+    sys.stdout.flush()
+
+    # Now that the dependencies are installed, the next step
+    # is to use cmake to build the corelib and wrapper in the build/corelib
+    # and build/wrapper directories
+
+    # change into the build/corelib directory
+    OLDPWD = os.getcwd()
+
+    build_ext = _get_build_ext()
+
+    build_dir = os.path.abspath("build")
+
+    coredir = os.path.join(build_dir, f"{build_ext}_corelib")
+
+    if not os.path.exists(coredir):
+        os.makedirs(coredir)
+
+    if not os.path.isdir(coredir):
+        print("SOMETHING IS WRONG. %s is not a directory?" % coredir)
+        sys.exit(-1)
+
+    os.chdir(coredir)
+
+    if os.path.exists("CMakeCache.txt"):
+        # we have run cmake in this directory before. Run it again.
+        status = subprocess.run([cmake, "."])
+    else:
+        # this is the first time we are running cmake
+        sourcedir = os.path.join(os.path.dirname(os.path.dirname(
+            os.getcwd())), "corelib")
+
+        if not os.path.exists(os.path.join(sourcedir, "CMakeLists.txt")):
+            print("SOMETHING IS WRONG. There is no file %s" % os.path.join(sourcedir, "CMakeLists.txt"))
+            sys.exit(-1)
+
+        for a in ("NetCDF_ROOT_DIR", "OPENMM_ROOT_DIR"):
+            for i, d in enumerate(args.corelib):
+                if (a in d[0]):
+                    v = args.corelib.pop(i)[0]
+                    if (not a in os.environ):
+                        os.environ[a] = v.split("=")[-1]
+        add_default_cmake_defs(args.corelib, ncores)
+        cmake_cmd = [cmake, *sum([["-D", d[0]] for d in args.corelib], []),
+                     *sum([["-G", g[0]] for g in args.generator], []),
+                     sourcedir]
+        if (CC):
+            os.environ["CC"] = CC
+        if (CXX):
+            os.environ["CXX"] = CXX
+        print(" ".join(cmake_cmd))
+        sys.stdout.flush()
+        status = subprocess.run(cmake_cmd)
+
+    if status.returncode != 0:
+        print("SOMETHING WENT WRONG WHEN USING CMAKE ON CORELIB!")
+        sys.exit(-1)
+
+    # Now that cmake has run, we can compile and install corelib
+    ######
+    ###### Compiling and installing corelib
+    ######
+    # Compile and install, as need to install to compile the wrappers
+    make_args = make_cmd(ncores, True)
+
+    print("NOW RUNNING \"%s\" --build . --target %s" % (cmake, " ".join(make_args)))
+    sys.stdout.flush()
+    status = subprocess.run([cmake, "--build", ".", "--target", *make_args])
+
+    if status.returncode != 0:
+        print("SOMETHING WENT WRONG WHEN COMPILING CORELIB!")
+        sys.exit(-1)
+
+    ######
+    ###### Compiling wrapper
+    ######
+
+    # Make sure that the Python in conda is used
+    pydefs.append([f"PYTHON_EXECUTABLE={python_exe}"])
+
+    os.chdir(OLDPWD)
+
+    wrapperdir = os.path.join(build_dir, f"{build_ext}_wrapper")
+
+    if not os.path.exists(wrapperdir):
+        os.makedirs(wrapperdir)
+
+    if not os.path.isdir(wrapperdir):
+        print("SOMETHING IS WRONG. %s is not a directory?" % wrapperdir)
+        sys.exit(-1)
+
+    os.chdir(wrapperdir)
+
+    if os.path.exists("CMakeCache.txt"):
+        # we have run cmake in this directory before. Run it again.
+        status = subprocess.run([cmake, "."])
+    else:
+        # this is the first time we are running cmake
+        sourcedir = os.path.join(os.path.dirname(os.path.dirname(
+                                                 os.getcwd())), "wrapper")
+
+        if not os.path.exists(os.path.join(sourcedir, "CMakeLists.txt")):
+            print("SOMETHING IS WRONG. There is no file %s" % os.path.join(sourcedir, "CMakeLists.txt"))
+            sys.exit(-1)
+
+        add_default_cmake_defs(args.wrapper, npycores)
+        cmake_cmd = [cmake, *sum([["-D", d[0]] for d in args.wrapper], []),
+                     *sum([["-G", g[0]] for g in args.generator], []),
+                     sourcedir]
+        print(" ".join(cmake_cmd))
+        sys.stdout.flush()
+        status = subprocess.run(cmake_cmd)
+
+    if status.returncode != 0:
+        print("SOMETHING WENT WRONG WHEN USING CMAKE ON WRAPPER!")
+        sys.exit(-1)
+
+    # Just compile the wrappers
+    make_args = make_cmd(npycores, False)
+
+    print("NOW RUNNING \"%s\" --build . --target %s" % (cmake, " ".join(make_args)))
+    sys.stdout.flush()
+    status = subprocess.run([cmake, "--build", ".", "--target", *make_args])
+
+    if status.returncode != 0:
+        print("SOMETHING WENT WRONG WHEN COMPILING THE WRAPPERS!")
+        sys.exit(-1)
+
+    os.chdir(OLDPWD)
+
+
+def install_module(ncores: int = 1):
+    print("\nInstalling the module")
+
+    OLDPWD = os.getcwd()
+
+    build_ext = _get_build_ext()
+
+    build_dir = os.path.abspath("build")
+
+    cmake = "cmake"
+
+    if is_windows:
+        cmake = f"{cmake}.exe"
+
+    cmake = os.path.join(conda_bin, cmake)
+
+    moduledir = os.path.join(build_dir, f"{build_ext}_module")
+
+    if not os.path.exists(moduledir):
+        os.makedirs(moduledir)
+
+    if not os.path.isdir(moduledir):
+        print("SOMETHING IS WRONG. %s is not a directory?" % moduledir)
+        sys.exit(-1)
+
+    os.chdir(moduledir)
+
+    if os.path.exists("CMakeCache.txt"):
+        # we have run cmake in this directory before. Run it again.
+        status = subprocess.run([cmake, "."])
+    else:
+        # this is the first time we are running cmake
+        sourcedir = os.path.join(os.path.dirname(os.path.dirname(
+            os.getcwd())), "src", "sire")
+
+        if not os.path.exists(os.path.join(sourcedir, "CMakeLists.txt")):
+            print("SOMETHING IS WRONG. There is no file %s" % os.path.join(sourcedir, "CMakeLists.txt"))
+            sys.exit(-1)
+
+        add_default_cmake_defs(args.wrapper, ncores)
+        cmake_cmd = [cmake, *sum([["-D", d[0]] for d in args.wrapper], []),
+                     *sum([["-G", g[0]] for g in args.generator], []),
+                     sourcedir]
+        print(" ".join(cmake_cmd))
+        sys.stdout.flush()
+        status = subprocess.run(cmake_cmd)
+
+    if status.returncode != 0:
+        print("SOMETHING WENT WRONG WHEN USING CMAKE ON MODULE!")
+        sys.exit(-1)
+
+    make_args = make_cmd(ncores, True)
+
+    # Now that cmake has run, we can compile and install wrapper
+    print("NOW RUNNING \"%s\" --build . --target %s" % (cmake, " ".join(make_args)))
+    sys.stdout.flush()
+    status = subprocess.run([cmake, "--build", ".", "--target", *make_args])
+
+    if status.returncode != 0:
+        print("SOMETHING WENT WRONG WHEN COMPILING WRAPPER!")
+        sys.exit(-1)
+
+
+def install(ncores: int = 1, npycores: int = 1):
     print("\nInstalling sire")
+
+    OLDPWD = os.getcwd()
+
+    build_ext = _get_build_ext()
+
+    build_dir = os.path.abspath("build")
+
+    wrapperdir = os.path.join(build_dir, f"{build_ext}_wrapper")
+
+    cmake = "cmake"
+
+    if is_windows:
+        cmake = f"{cmake}.exe"
+
+    cmake = os.path.join(conda_bin, cmake)
+
+    if not os.path.exists(wrapperdir):
+        os.makedirs(wrapperdir)
+
+    if not os.path.isdir(wrapperdir):
+        print("SOMETHING IS WRONG. %s is not a directory?" % wrapperdir)
+        sys.exit(-1)
+
+    os.chdir(wrapperdir)
+
+    # Now install the wrappers
+    make_args = make_cmd(npycores, True)
+
+    print("NOW RUNNING \"%s\" --build . --target %s" % (cmake, " ".join(make_args)))
+    sys.stdout.flush()
+    status = subprocess.run([cmake, "--build", ".", "--target", *make_args])
+
+    if status.returncode != 0:
+        print("SOMETHING WENT WRONG WHEN COMPILING THE WRAPPERS!")
+        sys.exit(-1)
+
+    os.chdir(OLDPWD)
+
+    install_module(ncores=ncores)
 
 
 if __name__ == "__main__":
@@ -298,15 +621,20 @@ if __name__ == "__main__":
             install_requires()
 
         if not args.skip_build:
-            build()
+            build(ncores=args.ncores, npycores=args.npycores,
+                  coredefs=args.corelib, pydefs=args.wrapper)
 
-        install()
+        install(ncores=args.ncores, npycores=args.npycores)
 
     elif action == "build":
         if not args.skip_deps:
             install_requires()
 
-        build()
+        build(ncores=args.ncores, npycores=args.npycores,
+              coredefs=args.corelib, pydefs=args.wrapper)
 
     elif action == "install_requires":
         install_requires()
+
+    elif action == "install_module":
+        install_module(ncores=args.ncores)
