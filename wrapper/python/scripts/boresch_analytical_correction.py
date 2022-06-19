279e91c13ae57706f29298efa602fc6d20d918cc
finlayclark <s1629668@ed.ac.uk>
2022-06-19 15:12:31 +0100

    Update Boresch analytical correction script help

--- wrapper/python/scripts/boresch_analytical_correction.py
+++ wrapper/python/scripts/boresch_analytical_correction.py
@@ -8,25 +8,26 @@ this is not the case, the semi-analytical expression should be used."""
 
 from Sire.Tools import BoreschAnalyticalCorrection
 from Sire.Tools import readParams
+import Sire.Config
 
 import argparse
 import sys
 import os
 
-parser = argparse.ArgumentParser(description="""Calculates the analytical correction for releasing Boresch restraints,
-                                             accounting for the standard state.""",
+parser = argparse.ArgumentParser(description="An app to calculate the analytical correction for releasing Boresch "
+                                             "restraints - see Equation 32 in J. Phys. Chem. B 2003, 107, 35, 9535–9551 "
+                                             "This includes the standard state correction and will be reliable when "
+                                             "restraints are sufficiently strong, r is sufficiently far from zero, "
+                                             "and r2, r1, l1, and r1, l1, l2 are sufficiently far from collinear. If "
+                                             "this is not the case, the semi-analytical expression should be used.",
                                 epilog="boresch_analytical_correction is built using Sire and is distributed "
                                         "under the GPL. For more information please visit "
                                         "http://siremol.org",
                                  prog="boresch_analytical_correction")
 
 parser.add_argument('-C', '--config', nargs="?",
-                    help='A config file used for the simulation (all are suitable)')
-
-parser.add_argument('-H', '--help-config', action="store_true",
-                    help="Get additional help regarding all of the parameters "
-                         "(and their default values) that can be "
-                         "set in the optionally-supplied CONFIG file")
+                    help='A config file used for the simulation (only the restraints '
+                         'dictionary and temperature are used to calculate the correction).')
 
 parser.add_argument('--author', action="store_true",
                     help="Get information about the authors of this script.")
@@ -43,8 +44,8 @@ args = parser.parse_args()
 must_exit = False
 
 if args.author:
-    print("""\nboresch_analytical_correction was written by Finlay Clark (C) 2022, but is very closely
-     based on standardstatecorrection, written by Julien Michel and Stefano Bosisio (C) 2017""")
+    print("\nboresch_analytical_correction was written by Finlay Clark (C) 2022, but is very closely"
+          "\nbased on standardstatecorrection, written by Julien Michel and Stefano Bosisio (C) 2017")
     must_exit = True
 
 if args.version:
