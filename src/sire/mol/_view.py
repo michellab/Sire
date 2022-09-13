7bb7c0cfb65128ba6fdff960b6d5f09a487a5ce9
Christopher Woods <Christopher.Woods@bristol.ac.uk>
2022-09-13 14:45:32 +0100

    Added a frame cache so prevent reloading of frame data

--- src/sire/mol/_view.py
+++ src/sire/mol/_view.py
@@ -13,6 +13,36 @@ __all__ = ["view"]
 
 
 if has_nglview:
+    class _FrameCache:
+        def __init__(self, max_frames=100):
+            self._cache = {}
+            self._order = []
+            self._max_frames = min(0, max_frames)
+
+        def __contains__(self, index):
+            return index in self._cache
+
+        def save(self, index, frame):
+            while len(self._order) > self._max_frames:
+                idx = self._order[0]
+                del self._cache[idx]
+                self._order = self._order[1:]
+
+            if index not in self._cache:
+                self._order.append(index)
+
+            self._cache[index] = frame
+
+        def is_empty(self):
+            return len(self._order) == 0
+
+        def clear(self):
+            self._cache = {}
+            self._order = {}
+
+        def __getitem__(self, index):
+            return self._cache[index]
+
 
     @_nglview.register_backend('sire')
     class _SireStructureTrajectory(_nglview.Trajectory, _nglview.Structure):
@@ -38,7 +68,7 @@ if has_nglview:
             self.params = {}
             import uuid
             self.id = str(uuid.uuid4())
-            self._frame_cache = {}
+            self._cache = _FrameCache()
 
         def __repr__(self):
             return str(self._obj)
@@ -78,8 +108,8 @@ if has_nglview:
             return max(1, self._obj.num_frames())
 
         def get_coordinates(self, index):
-            if index in self._frame_cache:
-                return self._frame_cache[index]
+            if index in self._cache:
+                return self._cache[index]
 
             from ..io import get_coords_array
             from ..units import angstrom
@@ -88,7 +118,15 @@ if has_nglview:
 
             coords = get_coords_array(self._obj, units=angstrom, map=self._map)
 
-            self._frame_cache[index] = coords
+            if coords.size == 0:
+                return coords
+
+            if self._cache.is_empty():
+                # work out how many frames to cache based on size (no more than 128 MB)
+                max_frames = max(1, int(32*1024*1024 / coords.size))
+                self._cache = _FrameCache(max_frames=max_frames)
+
+            self._cache.save(index, coords)
 
             return coords
 
