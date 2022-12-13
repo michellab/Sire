12981267ea8d83db9cbd088fb15a1d15581a5df2
Lester Hedges <lester.hedges@gmail.com>
2022-12-13 09:38:23 +0000

    Try removing build directory to free up space during conda build.

--- recipes/sire/bld.bat
+++ recipes/sire/bld.bat
@@ -1,1 +1,10 @@
+:: Sire build script for Windows.
+
+:: Build and install Sire.
 python setup.py install --skip-deps
+
+:: Exit immediately on error.
+if errorlevel 1 exit 1
+
+:: Remove the build files to free up space.
+rmdir /s/q build
