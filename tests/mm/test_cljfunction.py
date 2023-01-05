e6fb07b23ef25ac904cec3646ada2979079f9825
Christopher Woods <chryswoods@hey.com>
2023-01-05 19:56:09 +0000

    Adding a test of the low-level energy routines - this fails on Linux/ARM64.
    This should aid with debugging...

---
+++ tests/mm/test_cljfunction.py
@@ -0,0 +1,33 @@
+
+import pytest
+
+import sire as sr
+
+
+def test_cljfunction(ala_mols):
+    mols = ala_mols
+
+    # these are the two closest water molecules
+    mol0 = mols[187]
+    mol1 = mols[611]
+
+    # This is how (at a low level) we can convert properties
+    # into CLJAtoms (vector of coordinates, charges and LJ parameters)
+    # We set the molecule ID to 1
+    clj0 = sr.legacy.MM.CLJAtoms(mol0.property("coordinates").to_vector(),
+                                 mol0.property("charge").to_vector(),
+                                 mol0.property("LJ").to_vector(), 1)
+
+    # Same for the other molecule, except with molecule ID 2
+    clj1 = sr.legacy.MM.CLJAtoms(mol1.property("coordinates").to_vector(),
+                                 mol1.property("charge").to_vector(),
+                                 mol1.property("LJ").to_vector(), 2)
+
+    # Now create a CLJFunction to calculate CLJ energies with
+    # 15 A cutoff
+    func = sr.legacy.MM.CLJShiftFunction(15 * sr.units.angstrom)
+
+    nrgs = func.calculate(clj0, clj1)
+
+    assert nrgs[0] == pytest.approx(-10.90987)
+    assert nrgs[1] == pytest.approx(7.1808714)
